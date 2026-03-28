4abbf5ecf89a776f10b53738dab2972e953013a9
SankThomas <thomassankara74@gmail.com>
2026-03-28 08:47:38 +0300

    improve links

--- app/page.jsx
+++ app/page.jsx
@@ -4,11 +4,14 @@ import React from "react";
 export default function Home() {
   return (
     <div className="flex h-screen items-center justify-center gap-4 p-4">
-      <Link href="/dataverse" className="rounded border p-4">
-        Dataverse api
+      <Link href="/randomuser-client" className="rounded border p-4">
+        In a client component
       </Link>
-      <Link href="/randomuser" className="rounded border p-4">
-        Randomuser api
+      <Link href="/randomuser-server" className="rounded border p-4">
+        In a server component
+      </Link>
+      <Link href="/dataverse" className="rounded border p-4">
+        Using an API route
       </Link>
     </div>
   );
